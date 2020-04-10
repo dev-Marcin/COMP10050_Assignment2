b126891e244fb9989c20b0697da7fb76c88239d0
mango-1303 <marcinpl98@gmail.com>
2020-04-10 16:17:54 +0100

    Initial Focus project given in Labs

---
+++ Assignment2/cmake-build-debug/CMakeFiles/Focus.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/Users/Marcin/Desktop/Assignment2/game_init.c" "E:/Users/Marcin/Desktop/Assignment2/cmake-build-debug/CMakeFiles/Focus.dir/game_init.c.obj"
+  "E:/Users/Marcin/Desktop/Assignment2/input_output.c" "E:/Users/Marcin/Desktop/Assignment2/cmake-build-debug/CMakeFiles/Focus.dir/input_output.c.obj"
+  "E:/Users/Marcin/Desktop/Assignment2/main.c" "E:/Users/Marcin/Desktop/Assignment2/cmake-build-debug/CMakeFiles/Focus.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
